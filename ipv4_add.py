9cf21fe689603c56e2553fa498d648589a0b7157
irfankv <irfankv>
2020-02-28 19:51:18 +0530

    added few scripts

--- ipv4_add.py
+++ ipv4_add.py
@@ -24,7 +24,7 @@ for i in range(0, 1021):
 j = 0
 c = 0
 k = 0
-for i in range(1, 33):
+for i in range(1, 2):
     c = int(i % 255)
     for j in range(0, 256):
         k += 1
@@ -35,9 +35,12 @@ for i in range(1, 33):
         # )
 
 
-for i in range(1, 501):
+for i in range(2, 200):
     j = hex(i).split("x")[1]
-    # print(f"{i} permit ipv6 host 2001:10:10::{j} host 2001:20:20::2")
+    k = 200+i
+    print(f"{k} deny tcp host 2001:20:0:1::2 gt 1023 host 2001:10:0:1::{j} gt 1023")
+    #print(f"{i} deny tcp host 2001:20:0:1::2 gt 1023 host 2001:10:0:1::2 gt 1023")
+    # print(f"{i} deny ipv6 host 2001:100:10::{j} host 2001:100:20::2")
     # print(
     #     f"{i} permit tcp host 2001:11:11::{j} gt 1 host 2001:21:21::2 lt 9000 nexthop1 ipv6 2001:192:168::2 nexthop2 ipv6 2001:192:168:3::2 nexthop3 ipv6 2001:192:168:1::2"
     # )
@@ -45,11 +48,28 @@ for i in range(1, 501):
 #     print(f"{i} deny tcp host 20.0.0.2 eq {i} host 10.0.0.2 eq {i}")
 
 
-def func(meass):
-    meass_func = "Variable Inside func"
-    print(f"{meass_func}")
+# def func(meass):
+#     meass_func = "Variable Inside func"
+#     print(f"{meass_func}")
 
 
-# print(f"{meass_func}")
+# # print(f"{meass_func}")
 
-func("Irfan")
+# func("Irfan")
+
+
+# for i in range(2, 216):
+#     j = 10+i
+#     k = hex(i).split("x")[1]
+#     conf = f"""
+# interface Bundle-Ether100.{i}
+#  ipv4 address 10.{j}.1.2 255.255.0.0
+#  ipv6 address 2001:10:{j}:1::2/64
+#  encapsulation dot1q {i}
+#     """
+#     # print(conf)
+
+#     conf_static = f"""
+# 2001:100:11::{i}/128 Bundle-Ether100.{i} 2001:10:{j}:1::1
+#      """
+#     print(conf_static)
